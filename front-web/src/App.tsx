40cf27767ff67af91248ce659d4a8303d35974b2
ricaxixo <fogaca135@gmail.com>
2020-10-09 20:18:21 -0300

    Add Navbar

---
+++ front-web/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar';
+
+
+function App (){
+    return (
+        <Navbar />
+    );
+}
+export default App;
+
+
+/*const App = () => {
+    return (
+       
+<h1>App</h1>
+);
+    }
+
+
+export default App;*/
